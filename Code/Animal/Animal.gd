b2da5a4f83a2041eec1820a988892fd391b7d067
AspenTrees75 <startrk75@gmail.com>
2026-07-12 18:11:42 -0700

    added lerp

--- Code/Animal/Animal.gd
+++ Code/Animal/Animal.gd
@@ -1,22 +1,67 @@
 extends Node3D
 class_name Animal
 
-var leap_distance = 3.0
+@onready var graphics: Node3D = $Graphics
+
+# lerp(a, b, 0.5)
+#    16, 8, 0.5 = 4
+# lerp(position, next_spot, 0.25)
+
+
+var leap_distance: float = 1.0
+var weight: float = 1.0 # 0-1
+var weight_speed: float = 1.0 # per sec.
+
+var current_spot: Vector3
+var next_spot: Vector3
+
+
+func _ready() -> void:
+	current_spot = position
+	next_spot = position
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("move_left"):
-		position.x -= leap_distance
+		#position.x -= leap_distance
+		next_spot = current_spot + Vector3.LEFT
+		weight = 0.0
+		graphics.rotation_degrees.y = 90.0
 
 	if Input.is_action_just_pressed("move_right"):
-		position.x += leap_distance
+		#position.x += leap_distance
+		next_spot = current_spot + Vector3.RIGHT
+		weight = 0.0
+		graphics.rotation_degrees.y = -90.0
+		
 	if Input.is_action_just_pressed("move_up"):
-		position.z -= leap_distance
+		#position.z -= leap_distance
+		next_spot = current_spot + Vector3.FORWARD
+		weight = 0.0
+		graphics.rotation_degrees.y = 0.0
 		
 	if Input.is_action_just_pressed("move_down"):
-			position.z += leap_distance
-	pass
+		#position.z += leap_distance
+		next_spot = current_spot + Vector3.BACK
+		weight = 0.0
+		graphics.rotation_degrees.y = 180.0
 	
+	
+	
+	# Tweening
+	if weight < 1.0:
+		weight += weight_speed * delta
+	# Resting
+	else:
+		weight = 1.0
+		
+	print(weight)
+	
+	# Testing
+	next_spot = Vector3(1, 0 , 0)
+	position = lerp(current_spot, next_spot, weight)
 	
 	
 	
--- Code/Vehicle/Vehicle.gd
+++ Code/Vehicle/Vehicle.gd
@@ -2,8 +2,14 @@ extends Node3D
 class_name Vehicle
 
 var speed = 3.0 #mps
+# limit of travel
+var kill_distance: float = 10.0
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x += speed * delta
-	
+	position.z += speed * delta # forward
+	if position.z > kill_distance:
+		position.z = -kill_distance
+   	
+ 
