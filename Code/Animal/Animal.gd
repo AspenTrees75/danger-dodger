6acbb34f625c300081debfb5b8ad28e8888da704
AspenTrees75 <startrk75@gmail.com>
2026-07-20 15:44:24 -0700

    started hitboxes, started pause menu

--- Code/Animal/Animal.gd
+++ Code/Animal/Animal.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends Area3D
 class_name Animal
 
 @onready var graphics: Node3D = $Graphics
@@ -9,60 +9,68 @@ class_name Animal
 
 
 var leap_distance: float = 1.0
-var weight: float = 1.0 # 0-1
-var weight_speed: float = 1.0 # per sec.
+#var weight: float = 1.0 # 0-1
+#var weight_speed: float = 1.0 # per sec.
 
-var current_spot: Vector3
-var next_spot: Vector3
+#var current_spot: Vector3
+#var next_spot: Vector3
 
 
 func _ready() -> void:
-	current_spot = position
-	next_spot = position
-
-
-
+	#signal break
+	area_entered.connect(on_entered)
+	#current_spot = position
+	#next_spot = position
+	
+func on_entered(other_area) -> void:
+	if other_area is Vehicle:
+		print("Lose a Life")
+		
+	if other_area is Goal:
+		print("Goal!")
+		position = Vector3.ZERO #make dynamic
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("move_left"):
-		#position.x -= leap_distance
-		next_spot = current_spot + Vector3.LEFT
-		weight = 0.0
+		position.x -= leap_distance
+		#next_spot = current_spot + Vector3.LEFT
+		#weight = 0.0
 		graphics.rotation_degrees.y = 90.0
 
 	if Input.is_action_just_pressed("move_right"):
-		#position.x += leap_distance
-		next_spot = current_spot + Vector3.RIGHT
-		weight = 0.0
+		position.x += leap_distance
+		#next_spot = current_spot + Vector3.RIGHT
+		#weight = 0.0
 		graphics.rotation_degrees.y = -90.0
 		
 	if Input.is_action_just_pressed("move_up"):
-		#position.z -= leap_distance
-		next_spot = current_spot + Vector3.FORWARD
-		weight = 0.0
+		position.z -= leap_distance
+		#next_spot = current_spot + Vector3.FORWARD
+		#weight = 0.0
 		graphics.rotation_degrees.y = 0.0
 		
 	if Input.is_action_just_pressed("move_down"):
-		#position.z += leap_distance
-		next_spot = current_spot + Vector3.BACK
-		weight = 0.0
+		position.z += leap_distance
+		#next_spot = current_spot + Vector3.BACK
+		#weight = 0.0
 		graphics.rotation_degrees.y = 180.0
 	
 	
 	
 	# Tweening
-	if weight < 1.0:
-		weight += weight_speed * delta
+	#if weight < 1.0:
+		#weight += weight_speed * delta
 	# Resting
-	else:
-		weight = 1.0
-		current_spot = next_spot
+	#else:
+		#weight = 1.0
+		#current_spot = next_spot
 		
-	print(weight)
+	#print(weight)
 	
 	# Testing
-	next_spot = Vector3(1, 0 , 0)
-	position = lerp(current_spot, next_spot, weight)
+	#next_spot = Vector3(1, 0 , 0)
+	#position = lerp(current_spot, next_spot, weight)
 	
 	
 	
--- Code/Vehicle/Vehicle.gd
+++ Code/Vehicle/Vehicle.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends Area3D
 class_name Vehicle
 
 var speed = 3.0 #mps
